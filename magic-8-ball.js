6a386ac701cb82e2af369b7e8bd531ca4aad075b
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2019-10-17 09:38:13 -0400

    magic-8-ball-2
    The second and final version of The Magic Eight Ball Project from Khan Academy.

--- magic-8-ball.js
+++ magic-8-ball.js
@@ -3,24 +3,29 @@ ellipse(200, 200, 375, 375);
 fill(60, 0, 255);
 triangle(200, 104, 280, 280, 120, 280);
 var answer = floor(random (1, 6));
+fill(255, 255, 255);
 if (answer === 1) {
-    fill(173, 48, 169);
+    fill(255, 0, 255);
     text("YOU'LL HAVE", 164, 217);
     text("A MEDICORE", 166, 234);
     text("DAY", 190, 250); 
 } else if (answer === 2) {
+    fill(240, 39, 39);
     text("YOU'LL HAVE", 164, 217);
     text("TERRIBLE LUCK", 155, 234);
 } else if (answer === 3) {
+    fill(13, 255, 0);
     text("YOU'LL HAVE", 164, 217);
     text("EXTREME", 174, 231);
     text("GOOD LUCK", 168, 246);
 } else if (answer === 4) {
+    fill(255, 0, 0);
     text("YOU'LL HAVE", 164, 217);
     text("EXTREME", 174, 232);
     text("BAD LUCK", 174, 248);
 } else if (answer === 5) {
+    fill(255, 191, 0);
     text("YOUR DAY WILL", 156, 218);
     text("BE FULL OF", 170, 233);
     text("MANY SUPRISES", 156, 249);
-}
+} 
