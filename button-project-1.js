9275abcbb16e5548a5eb75eb713e63e2d8db5f27
Mdfs05 <55096826+Mdfs05@users.noreply.github.com>
2019-10-21 09:18:52 -0400

    button-challenge-2
    The final copy of The Button Challenge from Khan Academy.

--- button-project-1.js
+++ button-project-1.js
@@ -1,13 +1,9 @@
 draw = function() {
     fill(0, 255, 68); // start color
-    
-     if(mouseIsPressed && mouseX > 0 && mouseY < 200) {
+    if(mouseIsPressed && mouseY < 200) {
         fill(255, 0, 0);
     }
-    
     rect(0, 0, 400, 200);  // the button
-
-
 
     // The button text
     fill(0, 0, 0);
